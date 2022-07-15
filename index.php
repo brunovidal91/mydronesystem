b02c791eb4376502832051cd03be3f333547d3cf
Bruno Vidal <brunovidal.sky@gmail.com>
2022-07-15 19:30:29 -0300

    commit1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App Drones - Login Page</title>
+</head>
+<body>
+    <section>
+        <div class="container">
+            <div class="login-logo"><img src="img/drone2.png" alt="logo"></div>
+            <div class="login">
+                <form action="" method="POST">
+                    <input type="text" name="user" placeholder="E-mail" required autofocus autocomplete="off"/>
+                    <input type="password" name="pass" placeholder="Senha" required autocomplete="off"/>
+                    <input type="submit" value="Entrar"/>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
