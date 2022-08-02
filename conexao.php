689d8aad84afdf2fc76d726f3d277149855e7f97
Bruno Vidal <brunovidal.sky@gmail.com>
2022-08-02 19:39:21 -0300

    final

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$bd = "drones";
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
+$bd = "heroku_d52fd739477431f";
+$servidor = "us-cdbr-east-06.cleardb.net";
+$usuario = "b82bdb03d85ccb";
+$senha = "19cc502d";
 
 try{
     $conn =mysqli_connect($servidor, $usuario, $senha, $bd);
